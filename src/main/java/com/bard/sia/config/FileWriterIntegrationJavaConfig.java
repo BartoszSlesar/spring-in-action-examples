32cd45a66b123b07e9ff6a03844e4a0184efdda0
Bartosz <Bartosz>
2023-10-02 20:28:44 +0200

    added file integration examples

---
+++ src/main/java/com/bard/sia/config/FileWriterIntegrationJavaConfig.java
@@ -0,0 +1,43 @@
+package com.bard.sia.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.integration.annotation.Transformer;
+import org.springframework.integration.channel.DirectChannel;
+import org.springframework.integration.file.FileWritingMessageHandler;
+import org.springframework.integration.file.support.FileExistsMode;
+import org.springframework.integration.transformer.GenericTransformer;
+import org.springframework.messaging.MessageChannel;
+
+import java.io.File;
+
+
+public class FileWriterIntegrationJavaConfig {
+
+    @Bean
+    @Transformer(inputChannel = "textInChannel", outputChannel = "fileWriterChannel")
+    public GenericTransformer<String, String> upperCaseTransformer() {
+        return String::toUpperCase;
+    }
+
+    @Bean
+    @ServiceActivator(inputChannel = "fileWriterChannel")
+    public FileWritingMessageHandler fileWriter() {
+        FileWritingMessageHandler handler = new FileWritingMessageHandler(new File("/tmp/sia/files"));
+        handler.setExpectReply(false);
+        handler.setFileExistsMode(FileExistsMode.APPEND);
+        handler.setAppendNewLine(true);
+        return handler;
+    }
+
+//    Channels are created automatically by spring, by you can define it yourself like bellow.
+    @Bean
+    public MessageChannel textInChannel(){
+        return new DirectChannel();
+    }
+
+    @Bean
+    public MessageChannel fileWriterChannel(){
+        return new DirectChannel();
+    }
+}
